fad5d6439a9838bbf6e3a49361a867ea3672de6d
Ayesha-2019331105 <ayeshachowdhuryrimi@gmail.com>
2023-06-30 20:55:40 +0600

    secondCommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={'/product/${product.slug}'}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={'/product/${product.slug}'}>
+                  <p>{product.Name}</p>
+                </a>
+                <p>
+                  <strong>
+                    {product.price}
+                    tk.
+                  </strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
